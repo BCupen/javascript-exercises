e357b1cc91bd598e15993e158a99b7825ed36fd2
BCupen <bacupen@gmail.com>
2022-05-24 17:28:37 -0400

    9 and 10 finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const processedWord = word.toLowerCase().replace(/[^a-z]/g, "");
 
+    return (
+        processedWord.split("")
+        .reverse()
+        .join("") === processedWord
+    )
 };
 
 // Do not edit below this line
